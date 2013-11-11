ba5839f385aa006a9b9cda12ceb74e152d0b2612
Alex Thomas <alex.c.thomas@gmail.com>
2013-11-12 09:39:20 +1100

    updated twilio details

---
+++ config/initializers/twilio.rb
@@ -0,0 +1,9 @@
+Twilio.setup do |config|
+  config.app_root = app.root
+  config.twilio_account_sid = 'AC8fc65890767367f0e87131de1f4a84a4'
+  config.twilio_auth_token = '0d6d5eb2e4a42a352fe451438f486e0e'
+  config.twilio_app_id = 'AP260515ea3b72ce619a7a4f97d999f172'
+  config.twilio_phone_number = '+441254313324';
+  config.twilio_test_number = '+61404100419';
+  
+end
